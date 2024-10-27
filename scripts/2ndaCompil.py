1eb1d1e789c3b6b628739e3868b298037ff09892
Celes <celestemarmar2@gmail.com>
2024-10-27 19:17:05 -0300

    Progress before the jam

--- scripts/2ndaCompil.py
+++ scripts/2ndaCompil.py
@@ -1,4 +1,6 @@
-#if has 4 fields, then go with author/year/title/editorial format, if has 5 fields, then go with author/year/title/edition/editorial format
+#if detail has 4 fields, then go with author/year/title/editorial format; if has 5 fields, then go with author/year/title/edition/editorial format
+#if a register has 8 fields, then act as normal; if has 9, then a link has been provided, even tho when in this step weren't necesary
+
 def Main():
     pass
 
